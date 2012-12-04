a7e8d8276100254d7f0e7fb75dbab083b2dfa090
Jonathan Hefner <jonathan.hefner@gmail.com>
2012-12-04 14:15:55 -0500

    intial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+gem 'jquery-rails'
+gem 'sqlite3'
+
+gem 'clearance', '1.0.0.rc2'
+gem 'cancan'
+
+gem 'haml'
+gem 'haml-rails' # https://github.com/indirect/haml-rails
+gem 'formtastic'
+gem 'bourbon'
+gem 'neat'
+gem 'flutie'
+gem 'high_voltage'
+
+group :assets do
+  gem 'uglifier', '>= 1.0.3'
+  gem 'sass-rails',   '~> 3.2.3'
+  # gem 'coffee-rails', '~> 3.2.1'
+  # gem 'therubyracer', :platforms => :ruby
+end
+
+gem 'rspec-rails', group: [:development, :test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChicagoTeenMuseum::Application.config.secret_token = '13a02226b31cf244fbc9fbe38a4985fe4595b5ba26ca6a8f3a04c2244a48bccf2449911d3d43767cd9291da526adf2785bfcfa342810436cd5093abac3f6a017'
