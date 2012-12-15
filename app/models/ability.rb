d3ae000ddc40b2f3705b45b8ada74f49af0f8f4a
Jonathan Hefner <jonathan.hefner@gmail.com>
2012-12-14 19:14:37 -0500

    fix permissions for reading Users and Artworks

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,8 +8,8 @@ class Ability
       can :read, [Artwork, Gallery, User]
 
       if user
-        can :manage, Artwork, user_id: user.id
-        can :manage, User, id: user.id
+        can [:update, :destroy], Artwork, user_id: user.id
+        can [:update, :destroy], User, id: user.id
       else
         can :create, User, type: 'normal'
       end
