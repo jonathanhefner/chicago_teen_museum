3d1121bbf42935bc4c81a49598a77f81b9ccefb4
Jonathan Hefner <jonathan.hefner@gmail.com>
2013-03-19 11:46:43 -0500

    fix for asset precompilation for Heroku

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module ChicagoTeenMuseum
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Only partially load app when precompiling assets (Heroku requires this)
+    config.assets.initialize_on_precompile = false
   end
 end
